f79249ace093e2ec9a546a771bccff0160776209
James Bentley <james@hellicarandlewis.com>
2016-11-30 14:23:57 +0000

    Added addons and basic scene

---
+++ HairParty_v01/Assets/Scripts/AvatarTracker.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AvatarTracker : MonoBehaviour {
+
+    public GameObject[] basicAvatars;
+    public GameObject[] avatars;
+    public KinectManager manager;
+    public Camera cam;
+	// Use this for initialization
+	void Start () {
+        if(manager == null)
+        {
+            manager = GetComponent<KinectManager>();
+        }
+        if(cam == null)
+        {
+            cam = FindObjectOfType<Camera>();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCalibrate(int uidIndex)
+    {
+        Debug.Log("Creatung Player " + uidIndex);
+
+        basicAvatars[uidIndex].transform.localScale = new Vector3(1f, 1f, 1f);
+    }
+
+    void OnRemove(int uidIndex)
+    {
+        basicAvatars[uidIndex].transform.localScale = new Vector3(0f, 0f, 0f);
+
+    }
+}
